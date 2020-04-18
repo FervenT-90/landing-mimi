b5a4e85ca84e0a7bd6c7095a49ea17566511387d
Miguel Ángel Rodríguez <mige5553@gmail.com>
2020-04-18 10:10:04 +0100

    Init project and clean up

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+   <div class="container">
+      <h1>hola</h1>
+   </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+   components: {}
+});
+</script>
+
+<style></style>
---
+++ test/Logo.spec.js
@@ -0,0 +1,12 @@
+/* Sample of test */
+
+/* import { mount } from '@vue/test-utils'
+import Logo from '@/components/Logo.vue'
+
+describe('Logo', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(Logo)
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+})
+ */
