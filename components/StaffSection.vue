6b5189cc509230a543c37a34c03f8f0cd07f0ae4
Miguel Ángel Rodríguez <mige5553@gmail.com>
2020-04-25 19:01:34 +0100

    Created Staff section component.
    -Added the StaffSection.vue to Index.vue
    -Fixed margins in StaffCard.vue

--- components/StaffSection.vue
+++ components/StaffSection.vue
@@ -1,13 +1,19 @@
 <template>
-   <section
-      class="w-screen h-full md:h-screen md:flex-row flex flex-col justify-center items-center"
-   >
-      <staff-card />
-      <staff-card
-         img-src="https://images.unsplash.com/photo-1579119986241-5e72d26cb690?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
-         full-name="Mike Flanagang"
-         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-      />
+   <section>
+      <h1 class="text-center text-4xl text-white font-primary pt-6">
+         Our Team
+      </h1>
+
+      <div
+         class="my-3 mb-5 md:flex-row flex flex-col justify-center items-center"
+      >
+         <staff-card />
+         <staff-card
+            img-src="https://images.unsplash.com/photo-1579119986241-5e72d26cb690?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
+            full-name="Mike Flanagang"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+         />
+      </div>
    </section>
 </template>
 
