479efdb00ec422f9a8e58357edf304127093d689
Milan Matic <programming4live@gmail.com>
2020-04-26 12:05:11 +0100

    Created ScheduleSection component.
    - ScheduleElement.vue: created according to refactor
      MorningSchedule.vue and AfternoonSchedule.vue to one
      component.
    - PlansSection.vue: added some margin Y.
    - index.vue: added ScheduleSection to it.

--- components/ScheduleElement.vue
+++ components/ScheduleElement.vue
@@ -1,6 +1,6 @@
 <template>
-   <div class="flex flex-col items-center justify-center my-6 md:flex-row">
-      <img class="my-4 md:mr-24" :src="svg" alt="Afternoon icon" />
+   <div class="flex flex-col items-center justify-center mb-6 md:flex-row">
+      <img class="my-4 md:mr-24" :src="svgSrc" :alt="alt" />
       <ul class="text-lg text-center text-white font-secondary">
          <li v-for="(time, key) of timetable" :key="key">
             {{ time }}
@@ -12,17 +12,21 @@
 <script>
 export default {
    props: {
-      svg: {
+      svgSrc: {
          type: String,
-         default: '/svg/afternoon.min.svg'
+         default: '/svg/defaultSchedule.min.svg'
+      },
+      alt: {
+         type: String,
+         default: 'Default shedule icon'
       },
       timetable: {
          type: Array,
          default: () => [
-            '16:30 - 17:30',
-            '17:30 - 18:30',
-            '19:00 - 20:00',
-            '20:00 - 21:00'
+            '00:00 - 00:00',
+            '00:00 - 00:00',
+            '00:00 - 00:00',
+            '00:00 - 00:00'
          ]
       }
    }
---
+++ components/ScheduleSection.vue
@@ -0,0 +1,35 @@
+<template>
+   <section class="my-16">
+      <h1 class="pt-6 text-4xl text-center text-white font-primary">
+         Our Schedule
+      </h1>
+      <schedule-element
+         class="mt-3"
+         svg-src="/svg/morning.min.svg"
+         alt="Morning Icon"
+         :timetable="['09:00 - 10:00', '10:30 - 11:30', '11:00 - 13:00']"
+      />
+      <schedule-element
+         svg-src="/svg/afternoon.min.svg"
+         alt="Afternoon Icon"
+         :timetable="[
+            '16:30 - 17:30',
+            '17:30 - 18:30',
+            '19:00 - 20:00',
+            '20:00 - 21:00'
+         ]"
+      />
+   </section>
+</template>
+
+<script>
+import ScheduleElement from '@/components/ScheduleElement.vue';
+
+export default {
+   components: {
+      ScheduleElement
+   }
+};
+</script>
+
+<style scoped></style>
